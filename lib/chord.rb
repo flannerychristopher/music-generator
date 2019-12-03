5cf28e8fbaf12ad68108e95fdaf94543df4f09be
Christopher Flannery <flannerychristopher@gmail.com>
2019-12-03 12:50:07 -0700

    more gitignore and rubocop changes

--- lib/chord.rb
+++ lib/chord.rb
@@ -1,9 +1,8 @@
+# frozen_string_literal: true
+
 class Chord
-  def initialize(pitch: 0, pitches: [0, 2, 4, 7, 9])
+  def initialize pitch: 0, pitches: [0, 2, 4, 7, 9]
     @root_pitch = pitch % 12
     @pitches = pitches
   end
-
-
-
 end
